f3ac8d51d4d84241ea240e73dda02ed3ff26329e
AimanaTazhibaeva <Aimasha200300@gmail.com>
2022-06-29 23:16:12 -0700

    first initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React, {Component} from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Countries extends Component {
+  constructor(){
+    super();
+    this.state={
+       
+    }
+  }
+  render() {
+      return (
+        <div>
+          {this.props.loaded && <h3>Loading...</h3>}
+        
+        <ul>
+          {this.props.countries.map((country) => (
+            <li key={country.id} className="county">{country.name}</li>
+          )
+          )
+            }
+
+        </ul>
+        </div>
+      )
+  }
+}
+
+  class App extends Component {
+    constructor(){
+      super();
+      this.state={
+        countries : [],
+        filteredCountries : [],
+        searchTerm: "",
+        loaded: true
+      }
+    }
+
+
+    searchOnChange = (e) => {
+       this.setState({searchTerm:e.target.value})
+    }
+    componentDidMount(){
+      setTimeout(()=>{
+     this.fetchAPI()
+     this.setState({loaded:!this.state.loaded})
+      },2000)
+    }
+
+   
+    componentDidUpdate(prevProps,prevState){
+     if(prevState.searchTerm !== this.state.searchTerm ){
+        this.setState({
+           filteredCountries : this.state.countries.filter((item) => 
+            item.name.includes(this.state.searchTerm)
+           )
+        })
+     }
+    }
+
+    fetchAPI = () =>{
+      const url = "https://jsonplaceholder.typicode.com/users";
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          countries: data,
+          filteredCountries: data
+        })
+      })
+    }
+
+    render() {
+        return (
+          <div className="App"> 
+          <div onChange={this.searchOnChange} className="wrapper"> 
+          <input type="search" />
+          </div>
+       
+           <div className="main">
+             <Countries
+              countries={this.state.filteredCountries} 
+              loaded = {this.state.loaded}
+              />
+          </div>
+          </div>
+
+          
+        )
+    }
+
 }
 
 export default App;
